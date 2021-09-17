3f0a58a40cb2fc324d2dfaf07fe0b66ac2514679
Tyson Romero <romennoodles23@yahoo.com>
2021-09-17 10:48:59 -0600

    winner shown in DOM

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const newDeckBtn = document.querySelector('#new-deck-btn')
 const drawBtn = document.querySelector('#draw-btn')
 const cards = document.querySelector('.cards')
+const winnerTitle = document.querySelector('#winner-title')
 
 let deckId
 
-//function to get a new deck from 'deckofcards' API
+//function to GET a new deck from 'deckofcards' API with same id #
 const newDeck = () => {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
         .then(res => res.json())
@@ -13,9 +14,9 @@ const newDeck = () => {
         })
 }
 
-//function to draw 2 cards from 'deckofcards' API
+//function using fetch to draw 2 cards from 'deckofcards' API
 const drawCard = () => {
-    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)  //<== using template strings for GET to add deckId of new deck
         .then(res => res.json())
         .then(data => {
             //using template strings to create image element and insert drawn cards into respective outlines//
@@ -24,25 +25,26 @@ const drawCard = () => {
             `   
             cards.children[1].innerHTML = `
                 <img class="card-image" src=${data.cards[1].image}>
-            `   
+            `
+            let winnerText = whoWins(data.cards[0], data.cards[1])   
+            winnerTitle.textContent = winnerText
         })
-        whoWins()
 }
 
 //function to determine which player has the higher card//
 const whoWins = (card1, card2) => {
     const cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", 
-        "10", "JACK", "QUEEN", "KING", "ACE"]
+    "10", "JACK", "QUEEN", "KING", "ACE"]
 
-    const card1Value = cardValues.indexOf(card1.value)
+    const card1Value = cardValues.indexOf(card1.value) //<== getting card values based on index in array
     const card2Value = cardValues.indexOf(card2.value)
-    
-    if(card1Value > card2Value) {
-        console.log("Player 1 wins");
+    //simple if/else statement to determine winner
+    if (card1Value > card2Value) {
+        return "Computer WINS 😭";
     } else if (card2Value > card1Value) {
-        console.log("Player 2 wins");
+        return "You WIN! 🏆";
     } else {
-        console.log("It's a tie!");
+        return "It's WAR ⚔️";
     }
 }
 
@@ -50,5 +52,12 @@ const whoWins = (card1, card2) => {
 newDeckBtn.addEventListener('click', newDeck)
 drawBtn.addEventListener('click', drawCard)
 
+// let card1Obj = {
+//     value: "JACK"
+// }
+// let card2Obj = {
+//     value: "3"
+// }
 
+// whoWins(card1Obj, card2Obj)
 
